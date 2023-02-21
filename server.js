c3272290a0e4e4798d98e2aa6b6e9eb8eb5edcc4
Ali Abdullah <aliabdullah@Alis-MacBook-Air.local>
2023-02-21 01:48:45 +0100

    prod edits

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const matching = require('./Models/matching.js');
 const mongoose = require('mongoose');
 const { db } = require('./Models/matching.js');
 require('dotenv').config()
-PORT = 3000 || process.env.PORT
+port = process.env.PORT || 3000;
 app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
+app.listen(port, () => {
     console.log('Server successfully started!');
 });
 app.use(cors());
